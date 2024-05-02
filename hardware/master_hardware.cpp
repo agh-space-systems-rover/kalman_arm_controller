10030c7ac062d0b512cf4f637a9a98165eee7734
Kacper Iwicki <kacper.iwi@gmail.com>
2024-05-02 16:09:30 +0200

    Added master running node

--- hardware/master_hardware.cpp
+++ hardware/master_hardware.cpp
@@ -1,8 +1,3 @@
-#include <sensor_msgs/msg/joy.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <control_msgs/msg/joint_jog.hpp>
-#include <std_srvs/srv/trigger.hpp>
-#include <moveit_msgs/msg/planning_scene.hpp>
 #include <rclcpp/client.hpp>
 #include <rclcpp/experimental/buffers/intra_process_buffer.hpp>
 #include <rclcpp/node.hpp>
@@ -12,145 +7,67 @@
 #include <rclcpp/subscription.hpp>
 #include <rclcpp/time.hpp>
 #include <rclcpp/utilities.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <thread>
+#include "kalman_arm_controller/can_lib/can_driver.hpp"
+#include <future>
+#include <chrono>
 
-// We'll just set up parameters here
-const std::string JOY_TOPIC = "/joy";
-const std::string JOINT_TOPIC = "/servo_node/delta_joint_cmds";
-const std::string EEF_FRAME_ID = "ee";
-const std::string BASE_FRAME_ID = "arm_link";
+const uint16_t WRITE_CALLBACK_PERIOD_US = 10;
+const uint16_t READ_CALLBACK_PERIOD_US = 10;
 
-// Enums for button names -> axis/button array index
-// For XBOX 1 controller
-enum Axis
-{
-  LEFT_STICK_X = 0,
-  LEFT_STICK_Y = 1,
-  LEFT_TRIGGER = 2,
-  RIGHT_STICK_X = 3,
-  RIGHT_STICK_Y = 4,
-  RIGHT_TRIGGER = 5,
-  D_PAD_X = 6,
-  D_PAD_Y = 7
-};
-enum Button
-{
-  A = 0,
-  B = 1,
-  X = 2,
-  Y = 3,
-  LEFT_BUMPER = 4,
-  RIGHT_BUMPER = 5,
-  CHANGE_VIEW = 6,
-  MENU = 7,
-  HOME = 8,
-  LEFT_STICK_CLICK = 9,
-  RIGHT_STICK_CLICK = 10
-};
-
-// Some axes have offsets (e.g. the default trigger position is 1.0 not 0)
-// This will map the default values for the axes
-std::map<Axis, double> AXIS_DEFAULTS = { { LEFT_TRIGGER, 1.0 }, { RIGHT_TRIGGER, 1.0 } };
-std::map<Button, double> BUTTON_DEFAULTS;
-
-/** \brief // This converts a joystick axes and buttons array to a TwistStamped or JointJog message
- * @param axes The vector of continuous controller joystick axes
- * @param buttons The vector of discrete controller button values
- * @param twist A TwistStamped message to update in prep for publishing
- * @param joint A JointJog message to update in prep for publishing
- * @return return true if you want to publish a Twist, false if you want to publish a JointJog
- */
-bool convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& buttons,
-                     std::unique_ptr<geometry_msgs::msg::TwistStamped>& twist,
-                     std::unique_ptr<control_msgs::msg::JointJog>& joint)
-{
-  joint->joint_names.push_back("joint_1");
-  joint->velocities.push_back(axes[LEFT_STICK_X]);
-  joint->joint_names.push_back("joint_2");
-  joint->velocities.push_back(axes[LEFT_STICK_Y]);
-  joint->joint_names.push_back("joint_3");
-  joint->velocities.push_back(axes[RIGHT_STICK_Y]);
-  joint->joint_names.push_back("joint_4");
-  joint->velocities.push_back(axes[RIGHT_STICK_X]);
-  joint->joint_names.push_back("joint_6");
-  joint->velocities.push_back(axes[D_PAD_X]);
-  joint->joint_names.push_back("joint_5");
-  joint->velocities.push_back(axes[D_PAD_Y]);
-
-  return false;
-}
-
-/** \brief // This should update the frame_to_publish_ as needed for changing command frame via controller
- * @param frame_name Set the command frame to this
- * @param buttons The vector of discrete controller button values
- */
-void updateCmdFrame(std::string& frame_name, const std::vector<int>& buttons)
-{
-  if (buttons[CHANGE_VIEW] && frame_name == EEF_FRAME_ID)
-    frame_name = BASE_FRAME_ID;
-  else if (buttons[MENU] && frame_name == BASE_FRAME_ID)
-    frame_name = EEF_FRAME_ID;
-}
-
-namespace kalman_master_controller
+namespace kalman_master
 {
 
-class JoyToServoPub : public rclcpp::Node
+class CanMasterNode : public rclcpp::Node
 {
-public:
-  JoyToServoPub(const rclcpp::NodeOptions& options)
-    : Node("joy_from_master_republisher", options), frame_to_publish_(BASE_FRAME_ID)
-  {
-    // Setup pub/sub
-    joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-        JOY_TOPIC, rclcpp::SystemDefaultsQoS(),
-        [this](const sensor_msgs::msg::Joy::ConstSharedPtr& msg) { return joyCB(msg); });
-
-    twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(TWIST_TOPIC, rclcpp::SystemDefaultsQoS());
-    joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>(JOINT_TOPIC, rclcpp::SystemDefaultsQoS());
-    collision_pub_ =
-        this->create_publisher<moveit_msgs::msg::PlanningScene>("/planning_scene", rclcpp::SystemDefaultsQoS());
-
-    // Create a service client to start the ServoNode
-    servo_start_client_ = this->create_client<std_srvs::srv::Trigger>("/servo_node/start_servo");
-    servo_start_client_->wait_for_service(std::chrono::seconds(1));
-    servo_start_client_->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
-  }
-
-  ~JoyToServoPub() override
-  {
-    if (collision_pub_thread_.joinable())
-      collision_pub_thread_.join();
-  }
-
-  void joyCB(const sensor_msgs::msg::Joy::ConstSharedPtr& msg)
-  {
-    // Create the messages we might publish
-    auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
-
-    // This call updates the frame for twist commands
-    updateCmdFrame(frame_to_publish_, msg->buttons);
-
-    // Convert the joystick message to Twist or JointJog and publish
-    convertJoyToCmd(msg->axes, msg->buttons, twist_msg, joint_msg) joint_msg->header.stamp = this->now();
-    joint_msg->header.frame_id = "arm_link";
-    joint_pub_->publish(std::move(joint_msg));
-  }
-
 private:
-  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
-  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
-  rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
-  rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr collision_pub_;
-  rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr servo_start_client_;
+  MasterCanDriver can_driver_ = MasterCanDriver("can0");
+  std::future<void> writer;
+  rclcpp::TimerBase::SharedPtr write_timer_;
+  rclcpp::TimerBase::SharedPtr read_timer_;
 
-  std::string frame_to_publish_;
+  void writeCan()
+  {
+    if (writer.valid() && writer.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+    {
+      // RCLCPP_WARN(rclcpp::get_logger("my_logger"), "Previous write still in progress");
+    }
+    else
+    {
+      {
+        std::lock_guard<std::mutex> lock(this->can_driver_.m_write);
+        // Do something related to variables that write uses
+      }
+      // Run write in a separate thread
+      writer = std::async(std::launch::async, [&] { this->can_driver_.write(); });
+    }
+  }
 
-  std::thread collision_pub_thread_;
-};  // class JoyToServoPub
+  void readCan()
+  {
+    std::lock_guard<std::mutex> lock(this->can_driver_.m_read);
+    // Do something related to variables that read uses
+  }
 
-}  // namespace kalman_master_controller
+public:
+  CanMasterNode(const rclcpp::NodeOptions& options) : Node("can_master_node", options)
+  {
+    can_driver_.init();
+
+    write_timer_ = create_wall_timer(std::chrono::microseconds(WRITE_CALLBACK_PERIOD_US),
+                                     std::bind(&CanMasterNode::writeCan, this));
+    read_timer_ =
+        create_wall_timer(std::chrono::microseconds(READ_CALLBACK_PERIOD_US), std::bind(&CanMasterNode::readCan, this));
+  }
+
+  ~CanMasterNode() override
+  {
+  }
+};
+
+}  // namespace kalman_master
 
 // Register the component with class_loader
 #include <rclcpp_components/register_node_macro.hpp>
-RCLCPP_COMPONENTS_REGISTER_NODE(moveit_servo::JoyToServoPub)
+RCLCPP_COMPONENTS_REGISTER_NODE(kalman_master::CanMasterNode)
